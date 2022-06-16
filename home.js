0ab02fb6d78655185085fb1ba3f37123209af0d8
08210304 <107631132+08210304@users.noreply.github.com>
2022-06-17 03:06:22 +0800

    Add files via upload

---
+++ home.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $(".morechoco").click(function(){
+      $(".choco").slideToggle(500);
+    });
+  });
+
+$(document).ready(function(){
+    $(".moreyogurt").click(function(){
+      $(".yogurt").slideToggle(500);
+    });
+  });
+
+$(document).ready(function(){
+    $(".morecake").click(function(){
+      $(".cake").slideToggle(500);
+    });
+  });
